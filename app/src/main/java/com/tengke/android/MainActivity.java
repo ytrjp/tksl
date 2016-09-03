b4508bde7fc85d42ad3256890b6ae05f213ae2af
ZiMo <jinpeng@guangquaner.com>
2016-09-03 23:30:06 +0800

    add tow list view

--- app/src/main/java/com/tengke/android/MainActivity.java
+++ app/src/main/java/com/tengke/android/MainActivity.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -14,15 +11,14 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text_view) TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
 
 
-//        textView.setOnClickListener(v-> {
-//
-//        });
+//        textView.setText(R.string.hot_city);
+
     }
 }
