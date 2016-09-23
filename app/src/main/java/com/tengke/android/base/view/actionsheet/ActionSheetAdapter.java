f5ae19da8730ac3ac8e840452f78456fdffa93cc
ZiMo <jinpeng@guangquaner.com>
2016-09-23 23:23:56 +0800

    recommend & style

---
+++ app/src/main/java/com/tengke/android/base/view/actionsheet/ActionSheetAdapter.java
@@ -0,0 +1,73 @@
+package com.tengke.android.base.view.actionsheet;
+
+/**
+ * Created by jinpeng on 16/9/22.
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.tengke.android.R;
+
+import java.text.AttributedCharacterIterator;
+import java.util.jar.Attributes;
+
+/**
+ * Created by Clevo on 2016/6/7.
+ */
+public class ActionSheetAdapter extends BaseAdapter {
+
+    Context context;
+    String title[];
+
+    public ActionSheetAdapter(Context context, String[] title) {
+        this.context = context;
+        this.title = title;
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return title[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.actionsheet_adapter_pop, parent, false);
+            holder = new ViewHolder();
+            holder.pop_desp = (TextView) convertView.findViewById(R.id.pop_desp);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        TypedArray array = context.obtainStyledAttributes(null, R.styleable.SheetParams, R.attr.myThemeStyle, 0);
+        holder.pop_desp.setTextColor(array.getColor(R.styleable.SheetParams_textColor,
+                context.getResources().getColor(R.color.color_979797)));
+        holder.pop_desp.setTextSize(array.getDimensionPixelSize(R.styleable.SheetParams_textSize, 10));
+        holder.pop_desp.setText(title[position]);
+        array.recycle();
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        TextView pop_desp;
+    }
+}
--- app/src/main/java/com/tengke/android/business/hotels/HotelSearchActivity.java
+++ app/src/main/java/com/tengke/android/business/hotels/HotelSearchActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import com.tengke.android.R;
 import com.tengke.android.base.ui.BaseActivity;
+import com.tengke.android.base.view.actionsheet.ActionSheetFragment;
 import com.tengke.android.business.hotels.adapter.HotelSearchAdapter;
 
 import org.w3c.dom.Text;
@@ -14,17 +15,26 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HotelSearchActivity extends BaseActivity {
-    @BindView(R.id.list_view) ListView listView;
-    @BindView(R.id.recommend) TextView recommendTxt;
-    @BindView(R.id.price_star) TextView priceStarTxt;
-    @BindView(R.id.more) TextView moreText;
+    @BindView(R.id.list_view)
+    ListView listView;
+    @BindView(R.id.recommend)
+    TextView recommendTxt;
+    @BindView(R.id.price_star)
+    TextView priceStarTxt;
+    @BindView(R.id.more)
+    TextView moreText;
+
+    @BindArray(R.array.hotel_filter_recommend)
+    String[] filterRecommed;
 
     private HotelSearchAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +48,7 @@ public class HotelSearchActivity extends BaseActivity {
         recommendTxt.setOnClickListener(onClickListener);
 
         List<String> list = new ArrayList<>();
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add("酒店" + i);
         }
         mAdapter = new HotelSearchAdapter(this, list);
@@ -50,6 +60,18 @@ public class HotelSearchActivity extends BaseActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.recommend:
+                    ActionSheetFragment.build(getSupportFragmentManager())
+                            .setTitle("")
+                            .setVisiable(false)
+                            .setChoice(ActionSheetFragment.Builder.CHOICE.ITEM)
+                            .setTag("MainActivity")
+                            .setItems(filterRecommed)
+                            .setOnItemClickListener(new ActionSheetFragment.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(int position) {
+
+                                }
+                            }).show();
                     break;
                 case R.id.price_star:
                     break;
